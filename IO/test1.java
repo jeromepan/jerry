14596a304ee17f6055ab958f8155d4786bb8baba
jerry <lmsj02@gmail.com>
2017-10-04 01:25:12 +0700

    update
    update

---
+++ IO/test1.java
@@ -0,0 +1,27 @@
+package IO;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+
+public class test1 {
+    public static void main(String[]args) throws FileNotFoundException{
+        PrintWriter pw = new PrintWriter(new File("test.csv"));
+        StringBuilder sb = new StringBuilder();
+        sb.append("id");
+        sb.append(',');
+        sb.append("Name");
+        sb.append('\n');
+
+        sb.append("1");
+        sb.append(',');
+        sb.append("Prashant Ghimire");
+        sb.append('\n');
+
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("done!");
+    }
+}
